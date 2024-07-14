1f3750c70e45937afd55b3c54471081a730a19ec
littleGreenDragon <wantingting666@gmail.com>
2024-07-15 01:36:15 +0800

    添加tabBar

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,29 +1,56 @@
-import { Link, Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { Link, Outlet,useNavigate } from "react-router-dom";
 import {loadBillList, billListSelector} from '@/store/modules/BillListSlice';
 import {useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { TabBar } from "antd-mobile"
+import '@/pages/Layout/index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
 
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: 'year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 function Layout(){
-    let dispatch = useDispatch();
-    let billList = useSelector(billListSelector);
+    const dispatch = useDispatch();
+    const billList = useSelector(billListSelector);
+    const navigate = useNavigate();
     useEffect(()=>{
         dispatch(loadBillList());
     },[dispatch]);
+    function switchRoute(path){//切换菜单跳转路由
+        console.log(path);
+        navigate(path);
+    }
     return (
-        <div>
-            layout
-            <Outlet/>
-            <ul>
-                {
-                    billList.map(elem=><li key={elem.id}>{elem.useFor}</li>)
-                }
-            </ul>
-            <Link to=''>月</Link>
-            <Link to='/new'>新建</Link>
-            <Link to='year'>年</Link>
-
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                ))}
+                </TabBar>
+            </div>
         </div>
     )
 }
