a3af2f17766597de955008424661319a7422d2e8
littleGreenDragon <wantingting666@gmail.com>
2024-07-15 02:55:59 +0800

    添加计算区域的点击显示隐藏和箭头朝向

--- src/pages/Month/Month.js
+++ src/pages/Month/Month.js
@@ -1,48 +1,62 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import '@/pages/Month/index.scss'
+import { useState } from 'react'
+import classNames from 'classnames';
 
 const Month = () => {
-  return (
-    <div className="monthlyBill">
-      <NavBar className="nav" backArrow={false}>
-        月度收支
-      </NavBar>
-      <div className="content">
-        <div className="header">
-          {/* 时间切换区域 */}
-          <div className="date">
-            <span className="text">
-              2023 | 3月账单
-            </span>
-            <span className='arrow expand'></span>
-          </div>
-          {/* 统计区域 */}
-          <div className='twoLineOverview'>
-            <div className="item">
-              <span className="money">{100}</span>
-              <span className="type">支出</span>
+    let [show, setShow] = useState(false);
+
+    function confirm(data){
+        console.log(data);
+        console.log(data.getYear());
+        console.log(data.getMonth());
+        setShow(false);
+    }
+
+    return (
+        <div className="monthlyBill">
+        <NavBar className="nav" backArrow={false}>
+            月度收支
+        </NavBar>
+        <div className="content">
+            <div className="header" onClick={()=>setShow(!show)}>
+            {/* 时间切换区域 */}
+            <div className="date">
+                <span className="text">
+                2023 | 3月账单
+                </span>
+                {/* expand会让箭头朝上 */}
+                <span className={classNames('arrow',{expand:!show})}></span>
             </div>
-            <div className="item">
-              <span className="money">{200}</span>
-              <span className="type">收入</span>
+            {/* 统计区域 */}
+            <div className='twoLineOverview'>
+                <div className="item">
+                <span className="money">{100}</span>
+                <span className="type">支出</span>
+                </div>
+                <div className="item">
+                <span className="money">{200}</span>
+                <span className="type">收入</span>
+                </div>
+                <div className="item">
+                <span className="money">{200}</span>
+                <span className="type">结余</span>
+                </div>
             </div>
-            <div className="item">
-              <span className="money">{200}</span>
-              <span className="type">结余</span>
+            {/* 时间选择器 */}
+            <DatePicker
+                className="kaDate"
+                title="记账日期"
+                precision="month"
+                visible={show}
+                max={new Date()}
+                onClose={()=>setShow(false)}
+                onConfirm={confirm}
+            />
             </div>
-          </div>
-          {/* 时间选择器 */}
-          <DatePicker
-            className="kaDate"
-            title="记账日期"
-            precision="month"
-            visible={false}
-            max={new Date()}
-          />
         </div>
-      </div>
-    </div >
-  )
+        </div >
+    )
 }
 
 export default Month
