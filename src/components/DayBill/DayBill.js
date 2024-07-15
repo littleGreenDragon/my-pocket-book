85a3fa4be60c24af2ccdf173ea678e2872002934
littleGreenDragon <wantingting666@gmail.com>
2024-07-15 20:04:10 +0800

    添加点击显示隐藏详细信息

--- src/components/DayBill/DayBill.js
+++ src/components/DayBill/DayBill.js
@@ -1,8 +1,10 @@
 import classNames from 'classnames'
 import '@/components/DayBill/index.scss'
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
+import {billTypeToName} from '@/const/billType';
 
 const DayBill = ({timeList}) => {
+  //计算总值、支出、收入
   const [pay, earn, sum] = useMemo(()=>{
     let pay = 0;
     let earn = 0;
@@ -16,12 +18,15 @@ const DayBill = ({timeList}) => {
     return [pay, earn , pay+earn];
   },[timeList]);
 
+  //详细列表的展开和关闭
+  const [showDatil, setShowDatil] = useState(false);
+
   return (
-    <div className={classNames('dailyBill')}>
+    <div className={classNames('dailyBill')} onClick={()=>setShowDatil(!showDatil)}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{timeList[0]}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames('arrow',{expand:!showDatil})}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -37,6 +42,22 @@ const DayBill = ({timeList}) => {
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      <div className="billList" style={{display:showDatil?'block':'none'}}>
+        {
+          timeList[1].map(item => {
+            return (
+              <div className="bill" key={item.id}>
+                <div className="detail">
+                  <div className="billType">{billTypeToName[item.useFor]}</div>
+                </div>
+                <div className={classNames('money', item.type)}>
+                  {item.money.toFixed(2)}
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
